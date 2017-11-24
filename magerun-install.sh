7364f4cf057c949b2346e0991d4e4f68e49bf20c
Johann Reinke <johann.reinke@mirakl.com>
2017-11-24 11:53:43 +0100

    Disable root check

--- magerun-install.sh
+++ magerun-install.sh
@@ -5,4 +5,4 @@ rm -rf "$MAGE_ROOT_DIR/*"
 /n98-magerun.phar install --root-dir="$MAGE_ROOT_DIR" --installationFolder="$MAGE_ROOT_DIR" \
     --dbHost="$MAGE_DB_HOST" --dbUser="$MAGE_DB_USER" --dbPass="$MAGE_DB_PASS" --dbName="$MAGE_DB_NAME" \
     --installSampleData="$MAGE_WITH_SAMPLE_DATA" --magentoVersionByName="$MAGE_VERSION_NAME" \
-    --baseUrl="$MAGE_BASE_URL" --useDefaultConfigParams=yes $@
+    --baseUrl="$MAGE_BASE_URL" --useDefaultConfigParams=yes --skip-root-check $@
